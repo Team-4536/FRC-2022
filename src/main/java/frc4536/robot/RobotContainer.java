51d2615e363357e8e70b52558af67a2f2682f934
i-m-batman <i-m-batman@users.noreply.github.com>
2022-01-13 08:29:06 -0600

    Change package paths from frc to frc4536

--- src/main/java/frc4536/robot/RobotContainer.java
+++ src/main/java/frc4536/robot/RobotContainer.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc4536.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc4536.robot.commands.ExampleCommand;
+import frc4536.robot.subsystems.ExampleSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
