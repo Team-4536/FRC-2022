85a4748b2ed3858319ce4775cddf6f890a18fe3e
LeoRhode <leorhode86@gmail.com>
2022-01-21 16:02:05 -0600

    Create cargohamdler subsytem with motor

--- src/main/java/frc4536/robot/Constants.java
+++ src/main/java/frc4536/robot/Constants.java
@@ -12,4 +12,10 @@ package frc4536.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class CargoHandlerInfo {
+    public static final int CARGO_HANDLER_MOTOR_ID = 2;
+
+    }
+
+}
