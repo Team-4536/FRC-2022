95c18088c4af3d1b8e24c920a49d53e1304dd271
Sophieingersoll <singersoll001@stpaul.k12.mn.us>
2022-01-22 11:50:05 -0600

    Added a poses file and created ball poses.
    Added each ball pose for autos, degrees were put in with 0 as a place holder, this will be changed when we know auto paths.

---
+++ src/main/java/frc4536/Poses.java
@@ -0,0 +1,22 @@
+package frc4536;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
+public class Poses {
+    public static final Pose2d
+    BALL_ONE = new Pose2d(Units.inchesToMeters(33.0), Units.inchesToMeters(-144.5), Rotation2d.fromDegrees(0)),
+    BALL_TWO = new Pose2d(Units.inchesToMeters(-25.2), Units.inchesToMeters(-146.14), Rotation2d.fromDegrees(0)),
+    BALL_THREE = new Pose2d(Units.inchesToMeters(-124.4), Units.inchesToMeters(-83.7), Rotation2d.fromDegrees(0)),
+    BALL_FOUR = new Pose2d(Units.inchesToMeters(-145.38), Units.inchesToMeters(-29.3), Rotation2d.fromDegrees(0)),
+    BALL_FIVE = new Pose2d(Units.inchesToMeters(-126.7), Units.inchesToMeters(77.0), Rotation2d.fromDegrees(0)),
+    BALL_SIX = new Pose2d(Units.inchesToMeters(-81.66), Units.inchesToMeters(120.28), Rotation2d.fromDegrees(0)),
+    BALL_SEVEN = new Pose2d(Units.inchesToMeters(-33.0), Units.inchesToMeters(144.5), Rotation2d.fromDegrees(0)),
+    BALL_EIGHT = new Pose2d(Units.inchesToMeters(25.2), Units.inchesToMeters(146.14), Rotation2d.fromDegrees(0)),
+    BALL_NINE = new Pose2d(Units.inchesToMeters(124.4), Units.inchesToMeters(83.7), Rotation2d.fromDegrees(0)),
+    BALL_TEN = new Pose2d(Units.inchesToMeters(145.38), Units.inchesToMeters(29.3), Rotation2d.fromDegrees(0)),
+    BALL_ELEVEN = new Pose2d(Units.inchesToMeters(126.7), Units.inchesToMeters(-77.0), Rotation2d.fromDegrees(0)),
+    BALL_TWELVE = new Pose2d(Units.inchesToMeters(81.66), Units.inchesToMeters(-120.28), Rotation2d.fromDegrees(0))
+    ;
+}
