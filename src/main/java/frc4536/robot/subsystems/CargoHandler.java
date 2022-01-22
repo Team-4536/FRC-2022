12303a2184305ccda4aa89e1059b44a458706286
Quinn <skeltonquinn@gmail.com>
2022-01-22 12:06:55 -0800

    add get speed for cargo handler motor

--- src/main/java/frc4536/robot/subsystems/CargoHandler.java
+++ src/main/java/frc4536/robot/subsystems/CargoHandler.java
@@ -9,23 +9,22 @@ public class CargoHandler extends SubsystemBase {
  public CargoHandler(){
      m_cargoHandlerMotor = new Spark(Constants.CargoHandlerInfo.CARGO_HANDLER_MOTOR_ID);
     m_cargoHandlerMotor.setInverted(Constants.CargoHandlerInfo.MOTOR_IS_GOING_IN_OTHER_DIRECTION);
-
-
-
-
  }
 
 
  public void intakeCargo(){
      m_cargoHandlerMotor.set(Constants.CargoHandlerInfo.INTAKE_CARGO_SPEED);
-    
-
  }
 
  public void outputCargo(){
      m_cargoHandlerMotor.set(Constants.CargoHandlerInfo.OUTPUT_CARGO_SPEED);
  }
 
+ public double getCargoHandlerMotorsSpeed(){
+     return m_cargoHandlerMotor.get();
+ }
+
+ 
 
 
 
