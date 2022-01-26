37fdc9e49193e3101b05935fd4ff24cbf4e2360e
Cal Grove <calgrove1@gmail.com>
2022-01-26 17:59:37 -0600

    make gyroscope

--- src/main/java/frc4536/robot/subsystems/Gyroscope.java
+++ src/main/java/frc4536/robot/subsystems/Gyroscope.java
@@ -1,7 +1,13 @@
 package frc4536.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gyroscope extends SubsystemBase{
+    private AHRS m_gyroscope;
     
+    public Gyroscope(){
+        m_gyroscope = new AHRS();
+    }
 }
