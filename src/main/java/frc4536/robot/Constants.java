d3699b93cf7333f3aa3eb8c93c457bebf40813f4
hangatu <hangatuu.131@gmail.com>
2022-01-28 16:04:15 -0600

    Replace constants in DriveTrain per code review

--- src/main/java/frc4536/robot/Constants.java
+++ src/main/java/frc4536/robot/Constants.java
@@ -6,30 +6,36 @@ package frc4536.robot;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-   
-   public static final class DriveInfo{
-    public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 1;
-    public static final int BACK_LEFT_DRIVE_MOTOR_ID = 2;
-    public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 3;
-    public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 4;
 
-    public static final MotorType TYPE_BRUSH_MOTOR = MotorType.kBrushed ;
+   public static final class DriveInfo {
+      public static final int LEFT_FRONT_DRIVE_MOTOR_ID = 1;
+      public static final int LEFT_REAR_DRIVE_MOTOR_ID = 2;
+      public static final int RIGHT_FRONT_DRIVE_MOTOR_ID = 3;
+      public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 4;
 
-    public static final boolean LEFT_DRIVE_MOTOR_INVERT = false;
-    public static final boolean RIGHT_DRIVE_MOTOR_INVERT = true;
+      public static final MotorType DRIVE_MOTOR_BRUSHED_TYPE = MotorType.kBrushed;
 
-    public static final int NUM_1_DRIVE_MOTOR_ENCODER = 1;
-    public static final int NUM_2_DRIVE_MOTOR_ENCODER = 2;
+      public static final boolean LEFT_DRIVE_MOTORS_ARE_INVERTED = false;
+      public static final boolean RIGHT_DRIVE_MOTORS_ARE_INVERTED = true;
 
+      public static final int LEFT_DRIVE_MOTOR_ENCODER = 1;
+      public static final int RIGHT_DRIVE_MOTOR_ENCODER = 2;
+
+      public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = EncodingType.k2X;
    }
-   
 }
--- src/main/java/frc4536/robot/subsystems/DriveTrain.java
+++ src/main/java/frc4536/robot/subsystems/DriveTrain.java
@@ -11,7 +11,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import frc4536.robot.Constants;
+
+import frc4536.robot.Constants.DriveInfo;;
 
 public class DriveTrain extends SubsystemBase{
     private DifferentialDrive m_differentialDrive;
@@ -21,21 +22,23 @@ public class DriveTrain extends SubsystemBase{
     private MotorControllerGroup m_rightMotorControllerGroup;
 
     public DriveTrain(){
-        CANSparkMax frontLeftDriveMotor = new CANSparkMax(Constants.DriveInfo.FRONT_LEFT_DRIVE_MOTOR_ID, Constants.DriveInfo.TYPE_BRUSH_MOTOR);
-        CANSparkMax frontRightDriveMotor = new CANSparkMax(Constants.DriveInfo.FRONT_RIGHT_DRIVE_MOTOR_ID, Constants.DriveInfo.TYPE_BRUSH_MOTOR);
-        CANSparkMax backLeftDriveMotor = new CANSparkMax(Constants.DriveInfo.BACK_LEFT_DRIVE_MOTOR_ID, Constants.DriveInfo.TYPE_BRUSH_MOTOR);
-        CANSparkMax backRightDriveMotor = new CANSparkMax(Constants.DriveInfo.BACK_RIGHT_DRIVE_MOTOR_ID, Constants.DriveInfo.TYPE_BRUSH_MOTOR);
+        CANSparkMax frontLeftDriveMotor = new CANSparkMax(DriveInfo.LEFT_FRONT_DRIVE_MOTOR_ID, DriveInfo.DRIVE_MOTOR_BRUSHED_TYPE);
+        CANSparkMax frontRightDriveMotor = new CANSparkMax(DriveInfo.RIGHT_FRONT_DRIVE_MOTOR_ID, DriveInfo.DRIVE_MOTOR_BRUSHED_TYPE);
+        CANSparkMax backLeftDriveMotor = new CANSparkMax(DriveInfo.LEFT_REAR_DRIVE_MOTOR_ID, DriveInfo.DRIVE_MOTOR_BRUSHED_TYPE);
+        CANSparkMax backRightDriveMotor = new CANSparkMax(DriveInfo.RIGHT_REAR_DRIVE_MOTOR_ID, DriveInfo.DRIVE_MOTOR_BRUSHED_TYPE);
 
         m_leftMotorControllerGroup = new MotorControllerGroup(frontLeftDriveMotor, backLeftDriveMotor);
         m_rightMotorControllerGroup = new MotorControllerGroup(frontRightDriveMotor, backRightDriveMotor);
 
-        m_leftMotorControllerGroup.setInverted(Constants.DriveInfo.LEFT_DRIVE_MOTOR_INVERT);
-        m_rightMotorControllerGroup.setInverted(Constants.DriveInfo.RIGHT_DRIVE_MOTOR_INVERT);
+        m_leftMotorControllerGroup.setInverted(DriveInfo.LEFT_DRIVE_MOTORS_ARE_INVERTED);
+        m_rightMotorControllerGroup.setInverted(DriveInfo.RIGHT_DRIVE_MOTORS_ARE_INVERTED);
 
         m_differentialDrive = new DifferentialDrive(m_leftMotorControllerGroup, m_rightMotorControllerGroup);
 
-        m_leftDriveEncoder = new Encoder( Constants.DriveInfo.NUM_1_DRIVE_MOTOR_ENCODER,  Constants.DriveInfo.NUM_2_DRIVE_MOTOR_ENCODER, Constants.DriveInfo.LEFT_DRIVE_MOTOR_INVERT, EncodingType.k2X); 
-        m_rightDriveEncoder = new Encoder( Constants.DriveInfo.NUM_1_DRIVE_MOTOR_ENCODER,  Constants.DriveInfo.NUM_2_DRIVE_MOTOR_ENCODER, Constants.DriveInfo.RIGHT_DRIVE_MOTOR_INVERT, EncodingType.k2X);  
+        m_leftDriveEncoder = new Encoder( DriveInfo.LEFT_DRIVE_MOTOR_ENCODER, DriveInfo.RIGHT_DRIVE_MOTOR_ENCODER, 
+            DriveInfo.LEFT_DRIVE_MOTORS_ARE_INVERTED, DriveInfo.DRIVE_MOTOR_ENCODER_ENCODINGTYPE); 
+        m_rightDriveEncoder = new Encoder( DriveInfo.LEFT_DRIVE_MOTOR_ENCODER, DriveInfo.RIGHT_DRIVE_MOTOR_ENCODER, 
+            DriveInfo.RIGHT_DRIVE_MOTORS_ARE_INVERTED, DriveInfo.DRIVE_MOTOR_ENCODER_ENCODINGTYPE);  
     } 
 
     public void arcadeDrive(double speed, double driveTrainRotation){
