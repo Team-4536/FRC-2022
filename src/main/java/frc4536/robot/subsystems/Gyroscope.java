9a7edb1423f3a0c85c616ce92ea53f1bded7a22f
Quinn <skeltonquinn@gmail.com>
2022-01-29 12:58:23 -0600

    worked on some classes for gyroscopes

--- src/main/java/frc4536/robot/subsystems/Gyroscope.java
+++ src/main/java/frc4536/robot/subsystems/Gyroscope.java
@@ -7,7 +7,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Gyroscope extends SubsystemBase{
     private AHRS m_gyroscope;
     
+    public class GyroscopeRotationalData{
+       
+        public GyroscopeRotationalData(){
+            float yaw;
+            float roll;
+            float pitch;
+            
+
+        }
+    }
+    
     public Gyroscope(){
         m_gyroscope = new AHRS();
     }
-}
+
+    public void getGyroscopeValues(){
+        GyroscopeRotationalData.yaw = Gyroscope.getYaw();
+        GyroscopeRotationalData.pitch = Gyroscope.getPitch();
+        GyroscopeRotationalData.roll = Gyroscope.getRoll();
+
+    }
+} 
+
