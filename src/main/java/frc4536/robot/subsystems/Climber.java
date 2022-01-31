76a4ef525f096ff5bb3157d73b69a87d8069448b
CalGrove1 <calgrove1@gmail.com>
2022-01-31 16:55:15 -0600

    create climber subsystem without hardware

---
+++ src/main/java/frc4536/robot/subsystems/Climber.java
@@ -0,0 +1,17 @@
+package frc4536.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase{
+
+    public Climber(){
+        //TODO - create climber hardware
+    }
+
+    public void climbForward(){}
+
+    public void climbBackward(){}
+
+    public void stopClimbing(){}
+
+}
