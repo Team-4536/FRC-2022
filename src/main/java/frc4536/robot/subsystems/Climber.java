e3def15bc2ccc2b917943831f00603221524fa05
CalGrove1 <calgrove1@gmail.com>
2022-01-31 19:46:28 -0600

    make files for climber and make climber

--- src/main/java/frc4536/robot/subsystems/Climber.java
+++ src/main/java/frc4536/robot/subsystems/Climber.java
@@ -1,17 +1,23 @@
 package frc4536.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Climber extends SubsystemBase{
+public class Climber extends SubsystemBase {
 
-    public Climber(){
-        //TODO - create climber hardware
+    public Spark m_climbMotor;
+
+    public Climber() {
+        // TODO - create climber hardware
     }
 
-    public void climbForward(){}
+    public void climbForward(){
+    } 
 
-    public void climbBackward(){}
+    public void climbBackward() {
+    }
 
-    public void stopClimbing(){}
+    public void stopClimbing() {
+    }
 
 }
