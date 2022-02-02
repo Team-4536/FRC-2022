0a0e52ec43c46d9a96f7c29a396dffd05648623f
Kieran LeVitre <kieranlevitre@gmail.com>
2022-02-02 17:37:41 -0600

    calculates clicks per degree and inch

--- src/main/java/frc4536/robot/Constants.java
+++ src/main/java/frc4536/robot/Constants.java
@@ -12,4 +12,24 @@ package frc4536.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+// TODO: find clicks per wheel rotation to calculate clicks per inch and clicks per degree
+
+    public static final class RobotInfo{
+        private static final double DRIVE_WHEEL_DIAMETER = 6.0;
+        private static final double WHEEL_CIRCUMFERENCE_IN_INCHES = DRIVE_WHEEL_DIAMETER * Math.PI;
+        private static final double DRIVE_ENCODER_CLICKS_PER_ROTATION = 1.0;
+        public static final double CLICKS_PER_INCH = WHEEL_CIRCUMFERENCE_IN_INCHES / DRIVE_ENCODER_CLICKS_PER_ROTATION;
+
+       private static final double INCHES_WIDTH_OF_AXLE = 22.0;
+       private static final double INCHES_LENGTH_OF_AXLE = 24.0;
+        private static final double DIAGONAL_INCHES_BETWEEN_AXES = Math.sqrt((INCHES_WIDTH_OF_AXLE * INCHES_WIDTH_OF_AXLE) + (INCHES_LENGTH_OF_AXLE * INCHES_LENGTH_OF_AXLE)) ;
+        private static final double ROBOT_ROTATION_CIRCUMFERENCE = DIAGONAL_INCHES_BETWEEN_AXES * Math.PI;
+        public static final double DRIVE_ENCODER_CLICKS_PER_DEGREE = ROBOT_ROTATION_CIRCUMFERENCE / CLICKS_PER_INCH / 360;
+    }
+    
+
+}
+
+
