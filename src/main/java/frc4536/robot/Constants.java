e4c3d1a5b6e649e2d2fb169e6ee4163a78e82bde
i-m-batman <matt.mcknight@edmn.org>
2022-02-01 19:15:52 -0600

    Clean up DriveTrain Constants

--- src/main/java/frc4536/robot/Constants.java
+++ src/main/java/frc4536/robot/Constants.java
@@ -8,33 +8,25 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
 
    public static final class DriveInfo {
       public static final int LEFT_FRONT_DRIVE_MOTOR_ID = 1;
       public static final int LEFT_REAR_DRIVE_MOTOR_ID = 2;
-      public static final int RIGHT_FRONT_DRIVE_MOTOR_ID = 3;
-      public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 4;
+      public static final int RIGHT_FRONT_DRIVE_MOTOR_ID = 4;
+      public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 3;
 
       public static final MotorType DRIVE_MOTOR_BRUSHED_TYPE = MotorType.kBrushed;
 
-      public static final boolean LEFT_DRIVE_MOTORS_ARE_INVERTED = false;
-      public static final boolean RIGHT_DRIVE_MOTORS_ARE_INVERTED = true;
+      public static final boolean LEFT_DRIVE_MOTORS_ARE_INVERTED = true;
+      public static final boolean RIGHT_DRIVE_MOTORS_ARE_INVERTED = false;
 
-      public static final int LEFT_DRIVE_MOTOR_ENCODER = 1;
-      public static final int RIGHT_DRIVE_MOTOR_ENCODER = 2;
+      public static final double DIFFERENTIAL_DRIVE_DEADBAND = 0.08;
+
+      public static final int LEFT_DRIVE_ENCODER_CHANNEL_A = 0;
+      public static final int LEFT_DRIVE_ENCODER_CHANNEL_B = 1;
+      public static final int RIGHT_DRIVE_ENCODER_CHANNEL_A = 2;
+      public static final int RIGHT_DRIVE_ENCODER_CHANNEL_B = 3;
 
       public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = EncodingType.k2X;
    }
