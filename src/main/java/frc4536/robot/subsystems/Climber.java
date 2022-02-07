1ad575f6226f267d2518eb746a58308074f262a9
Gavriel Pappas <gpappas001@stpaul.k12.mn.us>
2022-02-07 17:12:27 -0600

    creates methods for forward, backward and stop

--- src/main/java/frc4536/robot/subsystems/Climber.java
+++ src/main/java/frc4536/robot/subsystems/Climber.java
@@ -1,23 +1,30 @@
 package frc4536.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc4536.robot.Constants.ClimberInfo;
 
 public class Climber extends SubsystemBase {
 
-    public Spark m_climbMotor;
+    public CANSparkMax m_climbMotor;
 
     public Climber() {
-        // TODO - create climber hardware
+        CANSparkMax climberMotor = new CANSparkMax(ClimberInfo.CLIMBER_MOTOR_ID, ClimberInfo.CLIMBER_MOTOR_BRUSHED_TYPE);
+        m_climbMotor = climberMotor;
     }
 
     public void climbForward(){
+        m_climbMotor.set(0.5);
     } 
 
     public void climbBackward() {
+        m_climbMotor.set(-0.5);
     }
 
     public void stopClimbing() {
+        m_climbMotor.set(0.0);
     }
 
 }
