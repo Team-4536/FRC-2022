b2a5b6fd8ef5b09e2b92e8a0189b29d0d035b457
Gavriel Pappas <gpappas001@stpaul.k12.mn.us>
2022-02-11 16:51:03 -0600

    added encoder and get encoder method into climber

--- src/main/java/frc4536/robot/subsystems/Climber.java
+++ src/main/java/frc4536/robot/subsystems/Climber.java
@@ -1,18 +1,24 @@
 package frc4536.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc4536.robot.Constants.ClimberInfo;
 
 public class Climber extends SubsystemBase {
 
-    public CANSparkMax m_climbMotor;
+    private final CANSparkMax m_climbMotor;
+    private final RelativeEncoder m_climberEncoder;
+    private double m_lastKnownPosition;
 
     public Climber() {
         m_climbMotor = new CANSparkMax(ClimberInfo.CLIMBER_MOTOR_ID, ClimberInfo.CLIMBER_MOTOR_BRUSHED_TYPE);
         m_climbMotor.setInverted(ClimberInfo.CLIMBER_MOTOR_IS_INVERTED);
+        m_climberEncoder = m_climbMotor.getEncoder();
+        m_lastKnownPosition = 0;
     }
 
     public void climbForward(){
@@ -25,6 +31,14 @@ public class Climber extends SubsystemBase {
 
     public void stopClimbing() {
         m_climbMotor.set(0.0);
+    }
+
+    public void updateLastKnownPosition(){
+        m_lastKnownPosition = getClimbEncoder();
+    }
+
+    public double getClimbEncoder() {
+        return m_climberEncoder.getPosition() + m_lastKnownPosition;
     }
 
     @Override
