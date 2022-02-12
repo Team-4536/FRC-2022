5f4353591a0ce0d445b07f9ca95c01b5b7a7ba75
Cal Grove <calgrove1@gmail.com>
2022-02-12 11:54:25 -0600

    enter ip into axis camera

--- src/main/java/frc4536/robot/Robot.java
+++ src/main/java/frc4536/robot/Robot.java
@@ -29,7 +29,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    //CameraServer.addAxisCamera(host);
+    CameraServer.addAxisCamera("10.45.36.15");
   }
 
   /**
