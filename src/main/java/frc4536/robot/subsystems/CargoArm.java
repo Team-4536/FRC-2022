6ec1f86bc6ed22d69f45d00124d707f9b19032c8
quinn <skeltonquinn@gmail.com>
2022-02-26 10:01:23 -0600

    🔥REMOVE: unecessary spaces/etc cargoarm subsystem

--- src/main/java/frc4536/robot/subsystems/CargoArm.java
+++ src/main/java/frc4536/robot/subsystems/CargoArm.java
@@ -1,13 +1,10 @@
 package frc4536.robot.subsystems;
 
-import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc4536.robot.Constants.CargoArmInfo;
-
-
 public class CargoArm extends SubsystemBase{
     private Spark m_cargoArmElbow;
     private Spark m_cargoArmShoulder;
@@ -19,26 +16,24 @@ public class CargoArm extends SubsystemBase{
         m_cargoArmShoulder = new Spark(CargoArmInfo.CARGO_ARM_SHOULDER_ID);
 
         m_cargoArmElbowEncoder = new Encoder(CargoArmInfo.CHANNEL_A_CARGO_ARM_ELBOW_ENCODER,
-            CargoArmInfo.CHANNEL_B_CARGO_ARM_ELBOW_ENCODER, CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_IS_INVERTED, 
-            CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_TYPE);
+                CargoArmInfo.CHANNEL_B_CARGO_ARM_ELBOW_ENCODER, CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_IS_INVERTED,
+                CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_TYPE);
         m_cargoArmShoulderEncoder = new Encoder(CargoArmInfo.CHANNEL_A_CARGO_ARM_SHOULDER_ENCODER,
-           CargoArmInfo.CHANNEL_B_CARGO_ARM_SHOULDER_ENCODER,
-           CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_IS_INVERTED,CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_TYPE);
+                CargoArmInfo.CHANNEL_B_CARGO_ARM_SHOULDER_ENCODER,
+                CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_IS_INVERTED, CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_TYPE);
     }
 
-    public void moveElbow(double power) {
+    public void moveElbow(double power){
         m_cargoArmElbow.set(power);
-
     }
-    public int getElbowPosition() {
-       return m_cargoArmElbowEncoder.get();
-    }
-
-    public int getShoulderPosition() {
-        return m_cargoArmShoulderEncoder.get();
-    }
-
-    public void moveShoulder(double power) {
+    public void moveShoulder(double power){
         m_cargoArmShoulder.set(power);
+    }
+
+    public int getElbowPosition(){
+        return m_cargoArmElbowEncoder.get();
+    }
+    public int getShoulderPosition(){
+        return m_cargoArmShoulderEncoder.get();
     }
 }
