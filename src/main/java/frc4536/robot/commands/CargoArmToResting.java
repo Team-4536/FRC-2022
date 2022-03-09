ce1b274320ed661deecd5f5481fbf888b06672db
MinuteBots <97267316+M4rcusBC@users.noreply.github.com>
2022-03-09 16:54:52 -0600

    add target elbow position code start

--- src/main/java/frc4536/robot/commands/CargoArmToResting.java
+++ src/main/java/frc4536/robot/commands/CargoArmToResting.java
@@ -2,7 +2,6 @@ package frc4536.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc4536.robot.Constants;
 import frc4536.robot.Constants.CargoArmInfo;
 import frc4536.robot.subsystems.CargoArm;
 
@@ -22,49 +21,17 @@ public class CargoArmToResting extends CommandBase  {
     public void execute() {
         double currentShoulderPos = m_cargoArmSubsystem.getShoulderPosition();
         int currentElbowPos = m_cargoArmSubsystem.getElbowPosition();
-    
-        /*
-            if(cs < 1st){
-              ce = 0
-              cs = 0.25
-            } else if (cs < 2nd){
-              if(ce < 1){
-                cs = 0
-                ce = 0.25
-              }else {
-                cs = 0.25
-                ce= 0
-              }
-            } else if (cs < 3rd) {
-              cs = 0.25
-              ce = -cs/0.6
-
-            } else{
-              cs = 0
-              ce = 0
-            } 
-        */
+        double targetElbowPos = (currentShoulderPos - CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION)*CargoArmInfo.CARGO_ARM_ELBOW_TO_SHOULDER_RATIO_ABOVE_INTERMEDIATE+CargoArmInfo.CARGOARM_ELBOW_INTERMEDIATE_POSITION;
 
         if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION){
-          if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_INTERMEDIATE_POSITION){
-            m_cargoArmSubsystem.moveElbow(-Constants.CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
-            m_cargoArmSubsystem.moveShoulder(0);
-          }
-          else if (currentElbowPos < CargoArmInfo.CARGOARM_ELBOW_INTERMEDIATE_POSITION){
-            m_cargoArmSubsystem.moveElbow(Constants.CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
-            m_cargoArmSubsystem.moveShoulder(0);
-          }
-          else {
-            m_cargoArmSubsystem.moveShoulder(-Constants.CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
-            m_cargoArmSubsystem.moveElbow(0);
-          }
+          
         }
         else if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_RESTING_POSITION){
           if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION){
-            m_cargoArmSubsystem.moveElbow(-Constants.CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
+            m_cargoArmSubsystem.moveElbow(-CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
           }
           else{
-            m_cargoArmSubsystem.moveShoulder(-Constants.CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
+            m_cargoArmSubsystem.moveShoulder(-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
           }
         }
         else {
