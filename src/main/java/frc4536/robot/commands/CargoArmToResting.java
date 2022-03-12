d9d8079efc093547b8f52855c04d068d5c2fce3a
gavriel pappas <gpappas001@stpaul.k12.mn.us>
2022-03-12 09:26:01 -0600

    fix unnecessary stuff in to resting command

--- src/main/java/frc4536/robot/commands/CargoArmToResting.java
+++ src/main/java/frc4536/robot/commands/CargoArmToResting.java
@@ -20,24 +20,21 @@ public class CargoArmToResting extends CommandBase  {
     @Override
     public void execute() {
         double currentShoulderPos = m_cargoArmSubsystem.getShoulderPosition();
-        int currentElbowPos = m_cargoArmSubsystem.getElbowPosition();
+        double currentElbowPos = m_cargoArmSubsystem.getElbowPosition();
         double targetElbowPos = (currentShoulderPos - CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION)*CargoArmInfo.CARGO_ARM_ELBOW_TO_SHOULDER_RATIO_ABOVE_INTERMEDIATE+CargoArmInfo.CARGOARM_ELBOW_INTERMEDIATE_POSITION;
 
         if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION){
-          m_cargoArmSubsystem.moveShoulder(-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
-          if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_INTAKE_ELBOW_POSITION){
-            m_cargoArmSubsystem.moveElbow(CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER * ((targetElbowPos - currentElbowPos)/(3000)));
-          }
+          m_cargoArmSubsystem.moveShoulder(-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);  
+          m_cargoArmSubsystem.moveElbow(CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER * ((targetElbowPos - currentElbowPos)/(3000)));
         }
         else if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_RESTING_POSITION){
           if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION){
             m_cargoArmSubsystem.moveElbow(-CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
           }
-          if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_RESTING_POSITION){
-            m_cargoArmSubsystem.moveShoulder(-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
-          }
+          m_cargoArmSubsystem.moveShoulder((-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER)/2);
         }
         else {
+          m_cargoArmSubsystem.moveShoulder(0.0);
           if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION){
             m_cargoArmSubsystem.moveElbow(-CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
           }
