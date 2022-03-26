f8d53ff3f24255a52e44539aa05e8a61f6385413
gavriel pappas <gpappas001@stpaul.k12.mn.us>
2022-03-26 14:52:56 -0500

    Add sensor to reset CargoArm Elbow encoder at rest

--- src/main/java/frc4536/robot/subsystems/CargoArm.java
+++ src/main/java/frc4536/robot/subsystems/CargoArm.java
@@ -3,60 +3,84 @@ package frc4536.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import frc4536.robot.Constants;
 import frc4536.robot.Constants.CargoArmInfo;
 
-public class CargoArm extends SubsystemBase{
+public class CargoArm extends SubsystemBase {
+
+    private DigitalInput m_elbowHome;
+
+    private AnalogInput m_infraredSensor;
 
     private Spark m_cargoArmElbow;
     private CANSparkMax m_cargoArmShoulder;
     private Encoder m_cargoArmElbowEncoder;
     private RelativeEncoder m_cargoArmShoulderEncoder;
 
-    public CargoArm(){
+    public CargoArm() {
         m_cargoArmElbow = new Spark(CargoArmInfo.CARGO_ARM_ELBOW_ID);
-        m_cargoArmShoulder = new CANSparkMax(CargoArmInfo.CARGO_ARM_SHOULDER_ID, CargoArmInfo.CARGOARM_SHOULDER_BRUSHED_TYPE);
-        m_cargoArmShoulder.setInverted(Constants.CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_IS_INVERTED);
-        m_cargoArmElbow.setInverted(Constants.CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_IS_INVERTED);
+        m_cargoArmShoulder = new CANSparkMax(CargoArmInfo.CARGO_ARM_SHOULDER_ID,
+                CargoArmInfo.CARGOARM_SHOULDER_BRUSHED_TYPE);
+        m_cargoArmShoulder.setInverted(CargoArmInfo.CARGO_ARM_SHOULDER_ENCODER_IS_INVERTED);
+        m_cargoArmElbow.setInverted(CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_IS_INVERTED);
 
         m_cargoArmElbow.setInverted(CargoArmInfo.CARGO_ARM_ELBOW_MOTOR_IS_INVERTED);
         m_cargoArmShoulder.setInverted(CargoArmInfo.CARGO_ARM_SHOULDER_MOTOR_IS_INVERTED);
 
+        m_elbowHome = new DigitalInput(CargoArmInfo.CARGO_ARM_ELBOW_HOME_ID);
+
+        m_infraredSensor = new AnalogInput(CargoArmInfo.CARGO_ARM_INFRARED_SENSOR_ID);
+
         m_cargoArmElbowEncoder = new Encoder(CargoArmInfo.CHANNEL_A_CARGO_ARM_ELBOW_ENCODER,
-                CargoArmInfo.CHANNEL_B_CARGO_ARM_ELBOW_ENCODER, 
+                CargoArmInfo.CHANNEL_B_CARGO_ARM_ELBOW_ENCODER,
                 CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_IS_INVERTED,
                 CargoArmInfo.CARGO_ARM_ELBOW_ENCODER_TYPE);
         m_cargoArmShoulderEncoder = m_cargoArmShoulder.getEncoder();
     }
 
-    public void moveElbow(double power){
+    public void moveElbow(double power) {
         m_cargoArmElbow.set(power);
     }
 
-    public void moveShoulder(double power){
+    public void moveShoulder(double power) {
         m_cargoArmShoulder.set(power);
     }
 
-    public double getElbowPosition(){
+    public double getElbowPosition() {
         return m_cargoArmElbowEncoder.get();
     }
-    
-    public double getShoulderPosition(){
+
+    public double getShoulderPosition() {
         return m_cargoArmShoulderEncoder.getPosition();
+    }
+
+    public boolean elbowIsHome() {
+        return !m_elbowHome.get();
+    }
+
+    public double infraredSensorGetValue() {
+        return m_infraredSensor.getVoltage();
     }
 
     @Override
     public void periodic() {
-        if (CargoArmInfo.CARGO_ARM_IN_DASHBOARD){
+        if (elbowIsHome()) {
+            m_cargoArmElbowEncoder.reset();
+        }
+        
+        if (CargoArmInfo.CARGO_ARM_IN_DASHBOARD) {
             SmartDashboard.putNumber("Elbow Position", getElbowPosition());
             SmartDashboard.putNumber("Shoulder Position", getShoulderPosition());
             SmartDashboard.putNumber("Elbow Motor", m_cargoArmElbow.get());
             SmartDashboard.putNumber("Shoulder Motor", m_cargoArmShoulder.get());
+            SmartDashboard.putBoolean("is elbow at home position", !m_elbowHome.get());
+            SmartDashboard.putNumber("Range Sensor", m_infraredSensor.getVoltage());
         }
     }
 }
