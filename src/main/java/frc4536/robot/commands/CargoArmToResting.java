47f166b0554f9f6c4721897a3305affaca52e607
hangatu <hangatuu.131@gmail.com>
2022-03-26 14:54:02 -0500

    Use sensor to end CargoArm elbow movement

--- src/main/java/frc4536/robot/commands/CargoArmToResting.java
+++ src/main/java/frc4536/robot/commands/CargoArmToResting.java
@@ -1,17 +1,22 @@
 package frc4536.robot.commands;
 
+import edu.wpi.first.hal.simulation.ConstBufferCallback;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import frc4536.robot.Constants;
 import frc4536.robot.Constants.CargoArmInfo;
 import frc4536.robot.subsystems.CargoArm;
 
 public class CargoArmToResting extends CommandBase {
 
     private final CargoArm m_cargoArm;
+    private boolean m_elbowHasGoneHome;
 
     public CargoArmToResting(CargoArm cargoArm) {
         m_cargoArm = cargoArm;
+        m_elbowHasGoneHome = false;
         addRequirements(m_cargoArm);
+        
     }
 
     @Override
@@ -19,13 +24,20 @@ public class CargoArmToResting extends CommandBase {
         double currentShoulderPos = m_cargoArm.getShoulderPosition();
         double currentElbowPos = m_cargoArm.getElbowPosition();
 
+        if (!m_elbowHasGoneHome) {
+            if (m_cargoArm.elbowIsHome()) {
+                m_elbowHasGoneHome = true;
+            }
+        }
+
+
         double targetElbowPos = (currentShoulderPos - CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION)
                 * CargoArmInfo.CARGO_ARM_ELBOW_TO_SHOULDER_RATIO_ABOVE_INTERMEDIATE
                 + CargoArmInfo.CARGOARM_ELBOW_INTERMEDIATE_POSITION;
 
-        double pidPowerElbowValue = -Math.min((CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER
-                * Math.abs(CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION - currentElbowPos))
-                / CargoArmInfo.CARGO_ARM_PID_ELBOW_VALUE,
+        double pidPowerElbowValue = -Math.min(Math.max((CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER
+                * Math.abs(CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION - 1000 - currentElbowPos))
+                / CargoArmInfo.CARGO_ARM_PID_ELBOW_VALUE, 0.2),
                 CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER);
         double pidPowerShoulderValue = -Math.min((CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER
                 * Math.abs(CargoArmInfo.CARGOARM_SHOULDER_RESTING_POSITION - currentShoulderPos))
@@ -34,27 +46,32 @@ public class CargoArmToResting extends CommandBase {
 
         if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_INTERMEDIATE_POSITION) {
             m_cargoArm.moveShoulder(-CargoArmInfo.CARGO_ARM_SHOULDER_DEFAULT_POWER);
+            SmartDashboard.putBoolean("has gone home", m_elbowHasGoneHome);
             m_cargoArm.moveElbow(
                     CargoArmInfo.CARGO_ARM_ELBOW_DEFAULT_POWER * ((targetElbowPos - currentElbowPos) / (3000)));
         } else if (currentShoulderPos > CargoArmInfo.CARGOARM_SHOULDER_RESTING_POSITION) {
-            if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION) {
+            if (!m_elbowHasGoneHome) {
                 m_cargoArm.moveElbow(pidPowerElbowValue);
             } else
                 m_cargoArm.moveElbow(0.0);
             m_cargoArm.moveShoulder(pidPowerShoulderValue);
         } else {
             m_cargoArm.moveShoulder(0.0);
-            if (currentElbowPos > CargoArmInfo.CARGOARM_ELBOW_RESTING_POSITION) {
+            if (!m_elbowHasGoneHome) {
                 m_cargoArm.moveElbow(pidPowerElbowValue);
             } else {
                 m_cargoArm.moveElbow(0.0);
             }
         }
+        SmartDashboard.putBoolean("has gone home", m_elbowHasGoneHome);
     }
 
     @Override
     public void end(boolean interrupted) {
         m_cargoArm.moveElbow(0.0);
         m_cargoArm.moveShoulder(0.0);
+        m_elbowHasGoneHome = false;
     }
+
+
 }
