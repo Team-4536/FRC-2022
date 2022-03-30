64959e3e96f5248a7d5b3eb0a0f1da45dd65009c
Quandale Dingle <QuandaleDingle@QuandaleDingle.QuandaleDingle>
2022-03-30 16:29:20 -0500

    finish first auto command

--- src/main/java/frc4536/robot/commands/autos/autostryone.java
+++ src/main/java/frc4536/robot/commands/autos/autostryone.java
@@ -2,6 +2,9 @@ package frc4536.robot.commands.autos;
 
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc4536.robot.commands.CargoArmToResting;
+import frc4536.robot.commands.CargoArmToUpper;
+import frc4536.robot.commands.OutputCargo;
 import frc4536.robot.subsystems.CargoArm;
 import frc4536.robot.subsystems.CargoHandler;
 import frc4536.robot.subsystems.DriveTrain;
@@ -10,5 +13,11 @@ public class autostryone extends SequentialCommandGroup {
 
     public autostryone  (DriveTrain driveTrain, CargoArm cargoArm, CargoHandler cargoHandler) {
         RunCommand dontMove = new RunCommand(()->driveTrain.stopDriving(),driveTrain);
+
+        addCommands(new CargoArmToUpper(cargoArm).deadlineWith(dontMove),
+                    new OutputCargo(cargoHandler).withTimeout(4).deadlineWith(dontMove),
+                    new CargoArmToResting(cargoArm).deadlineWith(dontMove),
+                    new RunCommand(() -> driveTrain.tankDrive(-0.2, -0.2), driveTrain).withTimeout(1),
+                    dontMove);
     }
 }
