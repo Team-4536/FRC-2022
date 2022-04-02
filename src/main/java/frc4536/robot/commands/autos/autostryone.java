5a6e2ee508236914059454fe87b9a52304adf13b
gavriel pappas <gpappas001@stpaul.k12.mn.us>
2022-04-02 09:52:22 -0500

    change output time in auto command

--- src/main/java/frc4536/robot/commands/autos/autostryone.java
+++ src/main/java/frc4536/robot/commands/autos/autostryone.java
@@ -15,7 +15,7 @@ public class autostryone extends SequentialCommandGroup {
     public autostryone  (DriveTrain driveTrain, CargoArm cargoArm, CargoHandler cargoHandler) {
 
         addCommands(new CargoArmToUpper(cargoArm).deadlineWith(getDontMove(driveTrain)),
-                    new OutputCargo(cargoHandler).withTimeout(4).deadlineWith(getDontMove(driveTrain)),
+                    new OutputCargo(cargoHandler).withTimeout(2).deadlineWith(getDontMove(driveTrain)),
                     new CargoArmToResting(cargoArm).deadlineWith(getDontMove(driveTrain)),
                     new RunCommand(() -> driveTrain.tankDrive(-0.5, -0.5), driveTrain).withTimeout(1),
                     getDontMove(driveTrain));
