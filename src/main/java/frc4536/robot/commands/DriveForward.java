408552dfedf04157028119b76030f5c787720ca6
gavriel pappas <gpappas001@stpaul.k12.mn.us>
2022-04-04 17:03:15 -0500

    change drive forward command

--- src/main/java/frc4536/robot/commands/DriveForward.java
+++ src/main/java/frc4536/robot/commands/DriveForward.java
@@ -1,23 +1,45 @@
 package frc4536.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc4536.robot.Constants.RobotInfo;
 import frc4536.robot.subsystems.DriveTrain;
+import frc4536.robot.subsystems.Gyroscope;
 
 public class DriveForward extends CommandBase{
 
     private DriveTrain m_driveTrain;
+    private Gyroscope m_gyroscope;
+    private PIDController m_pidController;
+    private double kP;
+    private double kI;
+    private double kD;
     private double m_speed;
     private double m_distanceInFeet;
-    private Timer m_timer;
     private double m_goalPosition;
 
-    public DriveForward(DriveTrain driveTrain, double speed, double distanceInFeet){
+    public DriveForward(DriveTrain driveTrain, double speed, double distanceInFeet, Gyroscope gyroscope){
+        kP = .0001;
+        kI = 0;
+        kD = 0;
         m_driveTrain = driveTrain;
-        m_goalPosition = (distanceInFeet * 12) * RobotInfo.CLICKS_PER_INCH + m_driveTrain.getLeftDriveEncoderCount();
-        addRequirements(m_driveTrain);
+        m_gyroscope = gyroscope;
+        m_gyroscope.resetGyroscope();
+        
+        m_pidController = new PIDController(kP, kI, kD);
+        m_distanceInFeet = distanceInFeet;
+
+        m_goalPosition = (m_distanceInFeet * 12.0) * RobotInfo.CLICKS_PER_INCH + m_driveTrain.getLeftDriveEncoderCount();
+        SmartDashboard.putNumber("distance in feet", m_distanceInFeet);
+        SmartDashboard.putNumber("goal positiion", m_goalPosition);
+        SmartDashboard.putNumber("left encoder count", m_driveTrain.getLeftDriveEncoderCount());
+        SmartDashboard.putNumber("clicks per inch", RobotInfo.CLICKS_PER_INCH);
+
+        addRequirements(m_driveTrain, m_gyroscope);
+
+
     }
 
     @Override
@@ -27,7 +49,17 @@ public class DriveForward extends CommandBase{
 
     @Override
     public void execute(){
-        m_driveTrain.drive(0.5, 0, 0);
+        double turningValue = (m_gyroscope.getAngle() * 0.15);
+        double speed = Math.abs(m_goalPosition - m_driveTrain.getLeftDriveEncoderCount()) * 0.0001;
+        double dspeed = m_pidController.calculate(m_goalPosition = m_driveTrain.getLeftDriveEncoderCount());
+
+        m_driveTrain.arcadeDriveNoSquaredInptus(0.6, turningValue);
+
+        SmartDashboard.putNumber("encoder value drive", m_driveTrain.getLeftDriveEncoderCount());
+        SmartDashboard.putNumber("target position", m_goalPosition);
+        SmartDashboard.putNumber("Distance in feet", m_distanceInFeet);
+        SmartDashboard.putNumber("speed test", speed);
+        SmartDashboard.putNumber("d speed test", dspeed);
     }
 
     @Override
